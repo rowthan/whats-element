af997a42f490c0f1ac1545bc4510705b9aab8384
rowthan <ranronghuano1@163.com>
2017-12-10 14:56:35 +0800

    使用gulp构建项目

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+var watch = require('gulp-watch');
+
+gulp.task('compress', function (cb) {
+    pump([
+            gulp.src('src/**/*.js'),
+            uglify(),
+            gulp.dest('dist')
+        ],
+        cb
+    );
+});
+
+gulp.task('watch', function () {
+   gulp.watch('src/**/*.js',['compress'])
+});
