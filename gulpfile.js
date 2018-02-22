479455b7886562d20de1e9dcf31a07e3af3703eb
rowthan <ranronghuano1@163.com>
2018-02-22 13:35:59 +0800

    简化代码量，长字符串变量替换，压缩至4kb以下。

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var pump = require('pump');
 var watch = require('gulp-watch');
-
 gulp.task('compress', function (cb) {
     pump([
             gulp.src('src/**/*.js'),
--- src/whatsElement.js
+++ src/whatsElement.js
@@ -1,30 +1,30 @@
 /**
  * Created by rowthan on 2017/12/9.
  */
-(function(w,undefined){
-    var doc = w.document,
-    rootBody = "body",
-    rootHTML = "html",
+(function(window,undefined){
+    var document = window.document,
     PREFIX = 'whats-element',
-    containerID = 'whats-element-tip-container',
+    containerID = PREFIX+'-tip-container',
+    deleteID = PREFIX+'-tip-delete',
+    copyID = PREFIX+"-copy",
+    uniqueContainer = PREFIX+'-unique-container',
+    uniqueID = PREFIX+'-unique-id',
     whatsElement = function (argument) {
         this.options = Object.assign({},{
             draw:true
         },argument)
-        this.lastClick = doc.body
-        this.focusedElement = null
+        this.lastClick = document.body
         var that = this
         /**点击其他地方时，清除*/
-        doc.addEventListener('click', function(event){
+        document.addEventListener('click', function(event){
             that.lastClick = event.target
-            if(that.focusedElement!=null && that.focusedElement!=event.target){
-              o.clean()
+            if(that.focusedElement!==that.lastClick){
+              prototype.clean()
             }
         })
     },
-    o = whatsElement.prototype
-    o.version = '1.0.1'
-    o.getUniqueId = function (element,parent) {
+    prototype = whatsElement.prototype
+    prototype.getUniqueId = function (element,parent) {
         element = element ? element : this.lastClick;
         if(!(element instanceof HTMLElement)){
             console.error("invalid input,not a HTML element");
@@ -42,14 +42,14 @@
           className = "",
           classList = element.classList || [];
           classList.forEach(function (item) {
-            className = className+"."+item;
+            className += "."+item;
           })
-        if(tag===rootBody || tag=== rootHTML){
+        if(tag==="body" || tag=== "html"){
             result.wid = tag;
             result.type= tag;
         }
         //location by id
-        if(id && doc.getElementById(id) === element){
+        if(id && document.getElementById(id) === element){
             var regExp= new RegExp("^[a-zA-Z]+") ;
             if(!parent){
                 result.wid = id;
@@ -61,12 +61,12 @@
             result.type = "byId"
         }
         //location by name
-        if(!result.wid && name && doc.getElementsByName(name)[0] === element){
+        if(!result.wid && name && document.getElementsByName(name)[0] === element){
             result.wid = name;
             result.type = "byName"
         }
         //location by class
-        if(!result.wid && className && doc.querySelector(tag+className)===element){
+        if(!result.wid && className && document.querySelector(tag+className)===element){
             result.wid = tag+className;
             result.type = "byClass"
         }
@@ -76,7 +76,7 @@
             if(name){
                 queryString += "[name='"+name+"']"
             }
-            if(doc.querySelector(queryString)===element){
+            if(document.querySelector(queryString)===element){
                 result.wid = queryString
                 result.type = "byValue"
             }
@@ -86,7 +86,7 @@
             var queryString = tag;
             queryString = className ? queryString +className: queryString
             queryString = name? queryString + "[name='"+name+"']": queryString
-            if(o.getTarget(queryString)===element){
+            if(prototype.getTarget(queryString)===element){
                 result.wid = queryString
                 result.type = "byMixed"
             }
@@ -96,7 +96,7 @@
             var queryString = tag
             queryString = className ? queryString +className: queryString
 
-            var elements = doc.querySelectorAll(queryString)
+            var elements = document.querySelectorAll(queryString)
             if(elements && elements.length>0){
                 var index = null
                 for(var i=0; i<elements.length; i++){
@@ -107,7 +107,7 @@
                 }
                 if(index){
                     queryString = queryString + ":nth-child("+index+")"
-                    if(doc.querySelector(queryString) === element){
+                    if(document.querySelector(queryString) === element){
                         result.wid = queryString
                         result.type = "byOrder"
                     }
@@ -126,7 +126,7 @@
                 targetQuery += className;
             }
             var queryString = parentQueryString+">"+targetQuery,
-              queryElements = doc.querySelectorAll(queryString);
+              queryElements = document.querySelectorAll(queryString);
             if(queryElements.length>1){
                 queryString = null;
                 var index = null;
@@ -138,7 +138,7 @@
                 }
                 if(index>=1){
                     queryString = parentQueryString+">"+ targetQuery + ":nth-child("+index+")";
-                    var queryTarget = doc.querySelector(queryString);
+                    var queryTarget = document.querySelector(queryString);
                     if(queryTarget!=element){
                         queryString = null;
                     }
@@ -148,108 +148,94 @@
             result.type = "byParent"
         }
 
-        this.focusedElement = o.getTarget(result.wid);
+        this.focusedElement = prototype.getTarget(result.wid);
         if(!parent && this.options.draw ){
             whatsElement.prototype.draw(result);
         }
         return result
     }
-    o.getTarget = function (queryString) {
-        return doc.getElementById(queryString) || doc.getElementsByName(queryString)[0] || doc.querySelector(queryString);
+    prototype.getTarget = function (queryString) {
+        return document.getElementById(queryString) || document.getElementsByName(queryString)[0] || document.querySelector(queryString);
     }
-    o.draw = function (result) {
-        var target = o.getTarget(result.wid);
+    prototype.draw = function (result) {
+        var target = prototype.getTarget(result.wid);
         if(!target){
-            console.error("不存在该HTML对象，无法绘制");
+            console.error("no this element:"+result.wid);
             return;
         }
-        var tip = doc.getElementById(containerID) ? doc.getElementById(containerID) :doc.createElement("aside")
-        tip.id = containerID
+        var tip = document.getElementById(containerID) ? document.getElementById(containerID) :createElement("aside",containerID);
         tip.innerHTML = ""
         tip.addEventListener("click",function (e) {
             e.stopPropagation();
         })
 
-        var deleteButton = doc.createElement("div")
-        deleteButton.id = "whats-element-tip-delete"
-        deleteButton.innerText = 'x';
+        var deleteButton = createElement("div",deleteID)
+        deleteButton.innerText = 'x'
         deleteButton.onclick = function () {
           whatsElement.prototype.clean();
         }
         tip.appendChild(deleteButton);
 
-        var tipQueryContainer = doc.createElement("div");
-        tipQueryContainer.id = "whats-element-unique-container";
+        var tipQueryContainer = createElement("div",uniqueContainer);
 
-        var query = doc.createElement("input");
+        var query = createElement("input",uniqueID);
         query.readOnly = true;
-        query.id = "whats-element-unique-id";
         query.className = result.type;
         query.value = result.wid;
 
-        var tipCopy = doc.createElement("div");
-        tipCopy.id = "whats-element-copy";
+        var tipCopy = createElement("div",copyID);
         tipCopy.setAttribute("query-type",result.type);
         tipCopy.innerText = "复制";
         tipCopy.onclick=function () {
             query.select();
-            doc.execCommand("Copy");
+            document.execCommand("Copy");
         };
 
         tipQueryContainer.appendChild(query);
         tipQueryContainer.appendChild(tipCopy);
-
         tip.appendChild(tipQueryContainer);
-
-        var element = doc.createElement("div");
-        element.id = "whats-element-inner-text";
-        element.innerText = o.getTarget(result.wid).innerText;
-        tip.appendChild(element);
 
         var
         top = target.getBoundingClientRect().top + target.offsetHeight,
-        tempLeft = target.getBoundingClientRect().left+w.screenX,
-        toLeft = tempLeft<100?tempLeft:tempLeft-(tip.offsetWidth|240)/2+target.offsetWidth/2-doc.body.getBoundingClientRect().left;
+        tempLeft = target.getBoundingClientRect().left+window.screenX,
+        toLeft = tempLeft<100?tempLeft:tempLeft-(tip.offsetWidth|240)/2+target.offsetWidth/2-document.body.getBoundingClientRect().left;
         tip.style.left = toLeft+"px";
-        tip.style.top = top+w.scrollY+10+"px";
-        doc.body.appendChild(tip);
+        tip.style.top = top+window.scrollY+10+"px";
+        document.body.appendChild(tip);
         revertStyle();
         target.classList.add('whats-element-active');
     };
-    o.clean = function () {
+    prototype.clean = function () {
         revertStyle();
-        var container = doc.getElementById(containerID);
+        var container = document.getElementById(containerID);
         if(container){
             container.outerHTML='';
         }
     };
 
     function revertStyle(){
-      doc.querySelectorAll("."+PREFIX+"-active").forEach(function(element){
+      document.querySelectorAll("."+PREFIX+"-active").forEach(function(element){
         element.classList.remove(PREFIX+'-active');
       })
     }
-
-    if (typeof w !== "undefined" && w !== null) {
-       w.whatsElement = whatsElement;
+    function createElement(type,id) {
+      var el = document.createElement(type?type:"div");
+      id?el.id = id:"";
+      return el;
     }
-    if (typeof w === "undefined" || w === null) {
-        this.whatsElement = whatsElement;
-    }
-    if (typeof module !== 'undefined' && module.exports) module.exports = whatsElement;
-    if (typeof define === 'function') define(function() { return whatsElement; });
 
-    var style = doc.createElement("style"),
-    styleString = "#whats-element-tip-container{position: absolute;white-space: nowrap;background: #333740;color: #8ed3fb;font-size: 14px;z-index: 1000;background-color: rgba(255, 255, 255,0.95);box-sizing: border-box;box-shadow: rgba(0, 0, 0, 0.2) 0px 1px 10px 3px;padding: 10px 20px;border-radius: 36px;}"
-    styleString += "#whats-element-tip-delete{cursor: pointer;position: absolute;top: -10px;width: 20px;height: 20px;left: calc(50% - 8px);clip-path: polygon(50% 0, 100% 50%, 50% 100%, 0 50%);background: #fff;text-align: center;}";
-    styleString += "#whats-element-tip-delete:hover{background:#fffbf0}";
-    styleString += "#whats-element-unique-container{display:flex;justify-content: space-around;}";
-    styleString += "#whats-element-unique-id{border:1px solid #d1d5da;border-radius:3px;box-shadow:inset 0 1px 2px rgba(27,31,35,0.075);background-color:#fff;line-height: 20px;text-indent:10px;}";
-    styleString += "#whats-element-copy{background:aliceblue;text-align: center;border-radius: 5px;cursor: pointer;}";
-    styleString += "#whats-element-copy::after{position: absolute;z-index: 1000000;padding: 0.1em 0.75em;color: #fff;text-align: center;text-shadow: none;text-transform: none;content:attr(query-type);background: #a88462;border-radius: 3px;opacity: 0;transition:all .5s}";
-    styleString += "#whats-element-copy:hover:after{display:inline-block;opacity: 1;}";
-    styleString += "#whats-element-inner-text{color:#ddd;max-width:200px;max-height:100px;overflow:hidden;text-overflow:ellipsis;}";
-    styleString += ".whats-element-active{outline: red 1px dashed !important}";
+    var style = createElement("style"),
+    styleString = "#"+containerID+"{position:absolute;color:#8ed3fb;font-size:14px;z-index:1000;background-color:rgba(255, 255, 255,0.9);box-sizing:border-box;box-shadow:rgba(0, 0, 0, 0.2) 0px 1px 10px 3px;padding: 10px 20px;border-radius: 36px;}"
+    styleString += "#"+deleteID+"{cursor:pointer;position:absolute;top:-10px;width:20px;height: 20px;left:calc(50% - 8px);clip-path: polygon(50% 0,100% 50%,50% 100%,0 50%);background:#fff;}";
+    styleString += "#"+deleteID+":hover{background:#fffbf0}";
+    styleString += "#"+uniqueContainer+"{display:flex;justify-content:space-around;}";
+    styleString += "#"+uniqueID+"{border:1px solid #d1d5da;box-shadow:inset 0 1px 2px rgba(27,31,35,0.075);text-indent:10px;}";
+    styleString += "#"+copyID+"{background:aliceblue;cursor: pointer;}";
+    styleString += "#"+copyID+"::after{position:absolute;z-index:1000000;padding: 0.1em 0.75em;color: #fff;text-align:center;text-shadow:none;text-transform:none;content:attr(query-type);background: #a88462;border-radius: 3px;opacity: 0;transition:all .5s}";
+    styleString += "#"+copyID+":hover:after{display:inline-block;opacity: 1;}";
+    styleString += ".whats-element-active{outline:red 1px dashed !important}";
     style.innerText = styleString
-    doc.head.appendChild(style)
+    document.head.appendChild(style)
+
+    window.whatsElement = whatsElement
 })(window)
